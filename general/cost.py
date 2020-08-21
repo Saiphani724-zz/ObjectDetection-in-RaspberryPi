fc131bbe1fec1a6f1d768a7e85cec0a514bc3f5e
saiphani724 <saiphani724@gmail.com>
2020-08-21 13:42:38 +0530

    removed unused code

--- general/cost.py
+++ general/cost.py
@@ -1,21 +1,12 @@
-ind_cost = {
-	"pi_board" : 5699,
-	"case" : 387,
-	"hdmi_cable" : 320,
-	"memory_card" : 1210,
-	"charger": 894
-}
-
 kit_set_cost = {
 	"kit" : 6899,
 	"case for cooling" : 387,
 	"memory_card" : 1210,
+	"camera": 1000
 }
 
-tot1 = 0
-tot2 = 0
+tot = 0
 
-for item,cost in ind_cost.items(): tot1 += cost
-for item,cost in kit_set_cost.items(): tot2 += cost
-print(tot1,tot2)
+for item,cost in kit_set_cost.items(): tot += cost
+print(tot)
 
